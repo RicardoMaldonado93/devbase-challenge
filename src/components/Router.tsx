b4108bb0d1e0d512b0bd5fb2e08a70a37c25e64b
Ricardo Maldonado <maldonadoricardo93@gmail.com>
2021-11-02 09:31:09 -0300

    add animations in router

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,21 +1,34 @@
 import { Home } from "pages/Home";
 import { Profile } from "pages/Profile";
 import { Route, Redirect } from "react-router-dom";
+import { AnimatedSwitch } from "react-router-transition";
 
 export const Router = () => {
   return (
     <Route>
-      <Route path="/home" render={() => <Home />} key={"home"} />
-      <Route
-        path="/profile/:username"
-        render={() => <Profile />}
-        key={"profile"}
-      />
-      <Redirect path="/" to="/home" key={"redirect"} />
+      <AnimatedSwitch
+        atEnter={{ offset: 100 }}
+        atLeave={{ offset: -100 }}
+        atActive={{ offset: 0 }}
+        mapStyles={(styles) => ({
+          transform: `translateX(${styles.offset}%)`,
+          height: "100%",
+          width: "100%",
+          position: "absolute",
+        })}
+      >
+        <Route path="/home" render={() => <Home />} key={"home"} />
+        <Route
+          path="/profile/:username"
+          render={() => <Profile />}
+          key={"profile"}
+        />
+        <Redirect path="/" to="/home" key={"redirect"} />
+      </AnimatedSwitch>
     </Route>
   );
 };
 
 export interface IParamsProps {
-    username:string
+  username: string;
 }
