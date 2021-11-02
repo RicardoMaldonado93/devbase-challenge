3506693ba8ad9502c4ac10435054286d4df94ecb
Ricardo Maldonado <maldonadoricardo93@gmail.com>
2021-11-02 09:21:35 -0300

    add spinner component

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Spinner = () => {
+  return (
+    <div className="position-fixed w-100 h-100 justify">
+      <div className="d-flex justify-content-center py-5 mt-2">
+        <div className="spinner-border" role="status">
+        </div>
+      </div>
+    </div>
+  );
+};
